8be7b5d9c6c826b5d7a804b2a28cb2b415ee3089
Sara Le Lait <sjlelait@gmail.com>
2022-12-14 16:12:20 -0700

    ready to submit

--- script.js
+++ script.js
@@ -8,12 +8,11 @@ let weatherData, userInput
 
 $("form").on("submit", handleGetData)
 function handleGetData(event) {
-  event.preventDefault()
+  event.preventDefault()  
   userInput = $input.val()
   $.ajax({
     url: "https://api.openweathermap.org/data/2.5/weather?q="+userInput+"&units=imperial&appid=d4d03de8b8361a6f344c6260a8387c48"
-  })
-  
+  })  
   .then(
     (data) => {
       weatherData = data
